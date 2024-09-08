c941cf427d4054c91b7cb18d65768301b2b7518c
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-09-08 16:12:50 -0400

    Added a PlayerStateMachine

---
+++ Scripts/PSM/States/Moving.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+namespace ExodusGame.Scripts.PSM.States;
+
+public partial class Moving : PlayerState
+{
+    public override void PhysicsUpdate(double delta)
+    {
+        var direction = GetInputDirection();
+        PSM.Player.Velocity = direction * PSM.Player.MovementSpeed;
+        PSM.Player.MoveAndSlide();
+    }
+    private static Vector2 GetInputDirection()
+    {
+        var x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+        var y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+        return new Vector2(x, y).Normalized();
+    }
+}
