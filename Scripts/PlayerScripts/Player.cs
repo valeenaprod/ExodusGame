f5f3f344be8e0acca5f006116c5132d8455719ee
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-09-08 16:16:13 -0400

    Cleaned the Player script to remove the references to the old Player controller

--- Scripts/PlayerScripts/Player.cs
+++ Scripts/PlayerScripts/Player.cs
@@ -1,33 +1,30 @@
-using System;
+using ExodusGame.Scripts.PlayerScripts;
 using Godot;
 
-namespace Bunkify.Scripts.PlayerScripts;
+namespace ExodusGame.Scripts.PlayerScripts;
 
 public partial class Player : CharacterBody2D
 {
+    private Node2D _inArea;
+
     // Setters
     private Area2D _interactionArea;
-    private PlayerController _playerController;
-    private Node2D _inArea;
-    
+    public float MovementSpeed { get; private set; } = 100f;
+
     // Player Properties
     public override void _Ready()
     {
         _interactionArea = GetNode<Area2D>("InteractionArea");
-        _playerController = GetNode<PlayerController>("PlayerController");
         _inArea = null;
 
         // Subscribe to events
-        _playerController.BodyMovement += BodyMovement;
         _interactionArea.AreaEntered += InteractionAreaOnAreaEntered;
         _interactionArea.AreaExited += InteractionAreaOnAreaExited;
     }
-    
+
 
     public override void _ExitTree()
     {
-        // Unsubscribe to events
-        _playerController.BodyMovement -= BodyMovement;
         // Interaction Area; Area Entered/Exited
         _interactionArea.AreaEntered -= InteractionAreaOnAreaEntered;
         _interactionArea.AreaExited -= InteractionAreaOnAreaExited;
@@ -36,25 +33,15 @@ public partial class Player : CharacterBody2D
     private void InteractionAreaOnAreaExited(Area2D area)
     {
         if (!area.GetParent().IsInGroup("interactable")) return;
-        Logger.Log($"Left {area.GetParent().Name} {area.Name}!", Name);
+       // Logger.Log($"Left {area.GetParent().Name} {area.Name}!", Name);
         _inArea = null;
     }
 
     private void InteractionAreaOnAreaEntered(Area2D area)
     {
         if (!area.GetParent().IsInGroup("interactable")) return;
-        Logger.Log($"Entered {area.GetParent().Name}{area.Name}", Name);
+     //   Logger.Log($"Entered {area.GetParent().Name}{area.Name}", Name);
         _inArea = area;
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        _playerController.UpdatePlayerPhysics(delta);
-    }
-
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        _playerController.InputUpdate(@event);
     }
 
     private void BodyMovement(Vector2 newVelocity)
