cc8dd5490e0d1cb8807106a197112cacffe9b2b1
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-09-08 16:18:50 -0400

    Interactable base script

--- Scripts/Interactable.cs
+++ Scripts/Interactable.cs
@@ -1,12 +1,18 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 
-namespace Bunkify.Scripts;
+namespace ExodusGame.Scripts;
 
 public partial class Interactable : Node2D
 {
+    [Signal] public delegate void OnInteractEventHandler();
     public bool IsInteractable { get; private set; } = true;
     public string ObjectName { get; protected set; }
-    
+
+    public override void _Ready()
+    {
+        OnInteract += Interact;
+    }
     public virtual void Interact()
     {
         GD.Print("Interacting with base object.");
