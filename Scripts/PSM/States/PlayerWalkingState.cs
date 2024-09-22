0e69473efc1c1e323a21e21d7c76c9492caf436c
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-09-22 15:23:34 -0400

    Added the PlayerWalkingState class

---
+++ Scripts/PSM/States/PlayerWalkingState.cs
@@ -0,0 +1,68 @@
+﻿using Godot;
+
+namespace ExodusGame.Scripts.PSM.States;
+
+public class PlayerWalkingState : PlayerState
+{
+    public PlayerWalkingState(PlayerStateMachine stateMachine) : base(stateMachine) {}
+
+    private Vector2 _velocity;
+
+    public override void Enter()
+    {
+        base.Enter();
+        #if DEBUG
+        Logger.Log("Player started walking");
+        #endif
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        #if DEBUG
+        Logger.Log("Player stopped walking");
+        #endif
+    }
+
+    public override void HandleInput(InputEvent @event)
+    {
+        _velocity = Vector2.Zero;
+        
+        // Process input for walking
+        // Use pre-defined actions from the Input Map
+        if (Input.IsActionPressed("move_up"))
+        {
+            _velocity.Y = -1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            _velocity.Y = 1;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            _velocity.X = -1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            _velocity.X = 1;
+        }
+        
+        // Normalize velocity to prevent faster diagonal movement
+        _velocity = _velocity.Normalized();
+    }
+
+    public override void UpdateState(double delta)
+    {
+        var player = stateMachine.Player;
+        if (_velocity != Vector2.Zero)
+        {
+            player.Position += _velocity * player.MovementSpeed * (float)stateMachine.GetProcessDeltaTime();
+        }
+        else
+        {
+            stateMachine.ChangeState(new PlayerIdleState(stateMachine));
+        }
+    }
+    
+
+}
