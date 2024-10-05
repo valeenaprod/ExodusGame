e52073dc0fb6742611314f21204bf33a43db267e
Valeena <64002698+valeenaprod@users.noreply.github.com>
2024-10-05 16:22:07 -0400

    Manually resolved leftover merge conflicts

--- Systems/Power/PowerGenerator.cs
+++ Systems/Power/PowerGenerator.cs
@@ -1,22 +1,35 @@
 ﻿using ExodusGame.Scripts.Interaction;
 using ExodusGame.Scripts.Utils;
 
-<<<<<<< HEAD:Systems/Power/PowerGenerator.cs
 namespace ExodusGame.Systems.Power;
 
 public partial class PowerGenerator : Interactable
-=======
-namespace ExodusGame.Scripts.Machine
->>>>>>> master:Scripts/Machine/PowerGenerator.cs
 {
-    public partial class PowerGenerator : Interactable
-    {
-        private bool _isActive;
-        public int PowerGeneratedPerSecond { get; private set; } = 50;
+    private bool _isActive;
+    public int PowerGeneratedPerSecond { get; private set; } = 50;
 
-        public override void _Ready()
+    public override void _Ready()
+    {
+        ObjectName = "Power Generator";
+    }
+
+    public override void _Process(double delta)
+    {
+        if (!_isActive) return;
+        // Increase the power of the generator
+        GameManager.Instance.IncreasePower((int)(PowerGeneratedPerSecond * delta));
+    }
+
+    // Switch the generator to on or off
+    public override void Interact()
+    {
+        SwitchGenerator();
+    }
+
+    private void SwitchGenerator()
+    {
+        switch (_isActive)
         {
-<<<<<<< HEAD:Systems/Power/PowerGenerator.cs
             case true:
                 _isActive = false;
                 Logger.Log("Generator has been turned off!");
@@ -25,37 +38,6 @@ namespace ExodusGame.Scripts.Machine
                 _isActive = true;
                 Logger.Log("Generator has been turned on!");
                 break;
-=======
-            ObjectName = "Power Generator";
-        }
-
-        public override void _Process(double delta)
-        {
-            if (!_isActive) return;
-            // Increase the power of the generator
-            GameManager.Instance.IncreasePower((int)(PowerGeneratedPerSecond * delta));
-        }
-
-        // Switch the generator to on or off
-        public override void Interact()
-        {
-            SwitchGenerator();
-        }
-
-        private void SwitchGenerator()
-        {
-            switch (_isActive)
-            {
-                case true:
-                    _isActive = false;
-                    Logger.GameLog("Generator has been turned off!");
-                    break;
-                case false:
-                    _isActive = true;
-                    Logger.GameLog("Generator has been turned on!");
-                    break;
-            }
->>>>>>> master:Scripts/Machine/PowerGenerator.cs
         }
     }
 }
